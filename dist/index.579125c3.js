3fb899f046f6580b04d227f85a1caaa8926a6271
Jamison Fox <fox.jamison@outlook.com>
2022-10-24 20:53:10 -0600

    initial commit

---
+++ dist/index.579125c3.js
@@ -0,0 +1,7 @@
+const title = document.querySelector("h1");
+function changeTitle() {
+    title.styles.color = "red";
+}
+title.addEventListener("click", changeTitle);
+
+//# sourceMappingURL=index.579125c3.js.map
